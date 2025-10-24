3816a769ff4d4dc58085389c8130e1980777c0e0
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-10-24 15:56:18 +0700

    Menambahkan gambar logo umy

--- app/src/main/java/com/example/prak3/UIPertama.kt
+++ app/src/main/java/com/example/prak3/UIPertama.kt
@@ -2,6 +2,7 @@ package com.example.prak3
 
 import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -89,6 +90,8 @@ fun ActivitasPertama(modifier: Modifier){
 
             }
         }
+
+
     }
 
 }
